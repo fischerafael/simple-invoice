fb0b7377912a35fb4e3030040e670ea67b462201
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-10-23 23:03:15 -0300

    feat: working on the layout

--- src/pages/PageAppNewInvoice/index.tsx
+++ src/pages/PageAppNewInvoice/index.tsx
@@ -14,19 +14,48 @@ export const PageAppNewInvoice = () => {
           p="8"
           fontFamily="serif"
         >
+          <Chakra.HStack w="full" justify="space-between" align="flex-start">
+            <Chakra.VStack
+              align="flex-start"
+              w="full"
+              spacing="0"
+              fontSize="10px"
+            >
+              <Chakra.Text>Invoice</Chakra.Text>
+              <Chakra.Text>#2324532</Chakra.Text>
+              <Chakra.Text>24 october 2022</Chakra.Text>
+            </Chakra.VStack>
+
+            <Chakra.VStack
+              align="flex-start"
+              w="full"
+              spacing="0"
+              fontSize="10px"
+            >
+              <Chakra.Text>Rafael Santos Fischer</Chakra.Text>
+              <Chakra.Text>
+                Cláudio Manoel Da Costa Street, 637, house 3
+              </Chakra.Text>
+              <Chakra.Text>rafaelsanfischer@gmail.com</Chakra.Text>
+              <Chakra.Text>062.099.565-89</Chakra.Text>
+            </Chakra.VStack>
+          </Chakra.HStack>
+
           <Chakra.VStack
             align="flex-start"
             w="full"
             spacing="0"
             fontSize="10px"
           >
-            <Chakra.Text>Rafael Santos Fischer</Chakra.Text>
+            <Chakra.Text>Bill To</Chakra.Text>
+            <Chakra.Text>Guildhawk</Chakra.Text>
             <Chakra.Text>
-              Cláudio Manoel Da Costa Street, 637, house 3
+              Head Office 18th and 19th Floors, 100 Bishopsgate,
             </Chakra.Text>
-            <Chakra.Text>rafaelsanfischer@gmail.com</Chakra.Text>
-            <Chakra.Text>062.099.565-89</Chakra.Text>
+            <Chakra.Text>London, UK</Chakra.Text>
           </Chakra.VStack>
+
+          <Chakra.VStack w="full"></Chakra.VStack>
         </Chakra.VStack>
       </Chakra.VStack>
     </Chakra.SimpleGrid>
